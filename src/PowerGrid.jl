a7a2a0e6703e0165f7a7183dc5d284913df853c1
Rodrigo Muniz <rodrigosilvamuniz@gmail.com>
2024-08-23 16:44:35 +0200

    [wip] Basic Grid Structure

--- src/PowerGrid.jl
+++ src/PowerGrid.jl
@@ -1,5 +1,87 @@
 module PowerGrid
 
-# Write your package code here.
+using PythonCall
+
+import PowerModels
+import PowerModelsDistribution
+
+export Grid
+
+const nx = Ref{Py}()
+const walkerlayout = Ref{Py}()
+
+Graph(; kwargs...) = nx[].Graph(; kwargs...)
+
+walker_layout(args...; kwargs...) = walkerlayout[].WalkerLayouting.layout_networkx(args...; kwargs...)
+
+mutable struct Grid
+    source::String
+    settings::Dict{Symbol, Any}
+    eng_powermodel::Dict
+    mat_powermodel::Dict
+    graph::Py
+    buses::OrderedSet{Int64}
+    branches::OrderedSet{Tuple{Int64,Int64}}
+    get_substation::Function
+    export_to_json::Function
+    export_to_opendss::Function
+    save_powermodels::Function
+    export_powerplot::Function
+    plot_graph::Function
+    Grid(source::String) = _grid(new(), source)
+    Grid(source::String, settings::Dict{Symbol, Any}; kwargs...) = _grid(new(), source, settings; kwargs...)
+end
+
+function _grid(grid::Grid, source::String)
+    settings = Dict{Symbol, Any}(
+        :vm_lb => 0.92,
+        :vm_ub => 1.05,
+        :tm_step => 0.00625
+    )
+    return _grid(grid, source, settings)
+end
+
+function _grid(grid::Grid, source::String, settings::Dict{Symbol, Any}; kwargs...)
+    grid.source = source
+    grid.settings = settings
+
+    grid.graph = Graph()
+
+    # if endswith(source, ".dss")
+    #     grid.eng_powermodel, grid.mat_powermodel = _import_from_opendss(source; settings...)
+    # elseif endswith(source, ".json")
+    #     grid.eng_powermodel, grid.mat_powermodel = _import_from_json(source)
+    # else
+    #     @error "Format not recognized for source."
+    #     error("Format not recognized for source.")
+    # end
+
+    # try
+    #     grid.graph = _graph(grid.mat_powermodel)
+    # catch e
+    #     if isa(e, AssertionError)
+    #         @error "Graph is not radial or not connected!"
+    #         _generate_grid_log(grid.mat_powermodel, dirname(grid.source))
+    #         rethrow()
+    #     end
+    # end
+    # _update_mat_powermodel!(grid.mat_powermodel, grid.graph)
+    # grid.buses = _buses(grid.graph)
+    # grid.branches = _branches(grid.graph)
+
+    # grid.export_to_json = json_file -> _export_to_json(grid.eng_powermodel, json_file)
+    # grid.save_powermodels = path -> _save_powermodels(grid.eng_powermodel, grid.mat_powermodel, path)
+    # grid.export_powerplot = filepath -> _export_powerplot(grid.mat_powermodel, filepath)
+    # grid.plot_graph = (args...; kwargs...) -> _plot_graph_with_args(grid.graph, args...; kwargs...)
+
+    # grid.export_to_opendss = dss_file -> _export_to_opendss(grid.eng_powermodel, dss_file)
+    # grid.get_substation = () -> _get_substation(grid.mat_powermodel)
+    return grid
+end
+
+function __init__()
+    nx[] = pyimport("networkx")
+    walkerlayout[] = pyimport("walkerlayout")
+end
 
 end
