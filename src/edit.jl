ce688c3f3eb21c7ee922a13f73cafb22fb79272e
Rodrigo Muniz <rodrigosilvamuniz@gmail.com>
2024-09-07 22:43:35 +0200

    Edit Loads

--- src/edit.jl
+++ src/edit.jl
@@ -1,4 +1,4 @@
-function edit_load!(grid::Grid, load_name, attr_name, attr_value; all_phases=true)
+function edit_load!(grid::Grid, load_name, attr_name, attr_value; all_phases=true, update=true)
     @debug("Editing load $(load_name) attribute $(attr_name) to value $(attr_value).")
 
     if all_phases
@@ -9,12 +9,24 @@ function edit_load!(grid::Grid, load_name, attr_name, attr_value; all_phases=tru
         grid.eng_powermodel["load"][load_name][attr_name] = attr_value
     end
 
+    if update
+        _update_grid!(grid)
+    end
+
+    return nothing
+end
+
+function edit_loads!(grid::Grid, loads_tuple::Vector{Tuple{AbstractString,AbstractString,Any}})
+    for (load_name, attr_name, attr_value) in loads_tuple
+        edit_load!(grid, load_name, attr_name, attr_value; all_phases=false, update=false)
+    end
+
     _update_grid!(grid)
 
     return nothing
 end
 
-function edit_gen!(grid::Grid, gen_name, attr_name, attr_value; all_phases=true)
+function edit_gen!(grid::Grid, gen_name, attr_name, attr_value; all_phases=true, update=true)
     @debug("Editing generator $(gen_name) attribute $(attr_name) to value $(attr_value).")
 
     if all_phases
@@ -23,6 +35,18 @@ function edit_gen!(grid::Grid, gen_name, attr_name, attr_value; all_phases=true)
         end
     else
         grid.eng_powermodel["generator"][gen_name][attr_name] = attr_value
+    end
+
+    if update
+        _update_grid!(grid)
+    end
+
+    return nothing
+end
+
+function edit_gens!(grid::Grid, gens_tuple::Vector{Tuple{AbstractString,AbstractString,Any}})
+    for (gen_name, attr_name, attr_value) in gens_tuple
+        edit_gen!(grid, gen_name, attr_name, attr_value; all_phases=false, update=false)
     end
 
     _update_grid!(grid)
